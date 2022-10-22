d1a63aed6e577c2bc7cb780b20af9d23d7d37875
Isaac Johnson <isaac.johnson@gmail.com>
2022-10-22 08:34:51 -0500

    Update combined Config on rebuild

---
+++ updateKConfigs.pl
@@ -0,0 +1,47 @@
+#!/bin/perl
+#
+
+my ($combined,$newint,$output) = @ARGV;
+
+open(FILEH,"$combined");
+@filec = <FILEH>;
+close(FILEH);
+
+my $newcad=`cat $newint | grep 'certificate-authority-data' | sed 's/^.*: //'`;
+my $clientcertdata=`cat $newint | grep 'client-certificate-data' | sed 's/^.*: //'`;
+my $clientkeydata=`cat $newint | grep 'client-key-data' | sed 's/^.*: //'`;
+
+for (my $i = 0; $i < scalar(@filec); $i += 1)
+{
+	#	print "$i\n";
+
+	#  certificate-authority-data
+	if (($filec[$i] =~ /server: https:\/\/73.242.50.46:25460/)||($filec[$i] =~ /server: https:\/\/192.168.1.81:6443/))
+	{
+		#print $filec[($i - 1)];
+		$filec[($i - 1)] =~ s/^(.*)data: .*/\1data: /;
+		chomp($filec[($i - 1)]);
+		#	print $filec[($i - 1)] . $newcad;
+	        $filec[($i - 1)] .= $newcad;
+	}
+	# client cert and key data
+	if ($filec[$i] =~ /^- name: mac81/) {
+	    $filec[$i+2] =~ s/^(.*)data: .*/\1data: /;
+	    chomp($filec[$i+2]);
+	    $filec[$i+2] .= $clientcertdata;
+	    $filec[$i+3] =~ s/^(.*)data: .*/\1data: /;
+	    chomp($filec[$i+3]);
+	    $filec[$i+3] .= $clientkeydata;
+        }
+}
+
+# print updated file
+open(FILEO,">$output");
+foreach my $line (@filec)
+{
+   print FILEO $line;
+}
+close(FILEO);
+
+exit 0;
+
