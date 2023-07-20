187232ea01f2ce0003909bd10d6ea0639b3674ef
Isaac Johnson <isaac.johnson@gmail.com>
2023-07-20 10:20:04 -0500

    add check, restore evals

--- findAndMergeBlogPosts.sh
+++ findAndMergeBlogPosts.sh
@@ -7,6 +7,15 @@ export GITHUB_TOKEN="$2"
 
 for PNUM in `gh pr list --json number -q '.[] | .number'`; do
    echo "PNUM: $PNUM yo"
+
+   export NOWD=`date +%Y-%m-%d | tr -d '\n'`
+   export DATES=`gh pr view 216 --json body | jq -r .body | grep 'post: ' | sed 's/.*post: \(....-..-..\).*/\1/g' | tr -d '\n'`
+
+   if [[ "$DATES" == "$NOWD" ]]; then
+      echo "TODAY!"
+   else
+      echo "Not Today"
+   fi
 done
 
 exit
