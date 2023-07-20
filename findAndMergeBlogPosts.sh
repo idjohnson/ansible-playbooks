eb825742b50a2d2d1b390aa8aa614f859c08d43c
Isaac Johnson <isaac.johnson@gmail.com>
2023-07-20 10:34:28 -0500

    merge if it is time

--- findAndMergeBlogPosts.sh
+++ findAndMergeBlogPosts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -x
-
 cd $1
 
+set +x
 export GITHUB_TOKEN="$2"
+set -x
 
 for PNUM in `gh pr list --json number -q '.[] | .number'`; do
    echo "PNUM: $PNUM"
@@ -12,7 +12,8 @@ for PNUM in `gh pr list --json number -q '.[] | .number'`; do
    export DATES=`gh pr view $PNUM --json body | jq -r .body | grep 'post: ' | sed 's/.*post: \(....-..-..\).*/\1/g' | tr -d '\n'`
 
    if [[ "$DATES" == "$NOWD" ]]; then
-      echo "TODAY!"
+      echo "TODAY! - merging now!"
+      gh pr merge $PNUM --squash
    else
       echo "Not Today"
    fi
