7359cb7052e223ba759d14ee8f722d57c95f9487
Isaac Johnson <isaac.johnson@gmail.com>
2026-03-24 07:36:00 -0500

    do not message if same

--- testNewIp.sh
+++ testNewIp.sh
@@ -9,7 +9,7 @@ if [ "$MYIP" = "$HARBOR" ]; then
    echo "Strings are Equal . $MYIP matches $HARBOR"
 
    # Notify
-   curl -X POST -H 'Content-Type: application/json' https://notify.tpk.pw/notify -d "{\"title\": \"AWX: homelab IP STILL GOOD\", \"message\": \"IP of h.f.s is $HARBOR and egress is still $MYIP\", \"priority\": 5, \"password\": \"$NOTIFYPASS\"}"
+   # curl -X POST -H 'Content-Type: application/json' https://notify.tpk.pw/notify -d "{\"title\": \"AWX: homelab IP STILL GOOD\", \"message\": \"IP of h.f.s is $HARBOR and egress is still $MYIP\", \"priority\": 5, \"password\": \"$NOTIFYPASS\"}"
 else
    echo "IP CHANGED!  Harbor $HARBOR does not match local $MYIP"
 
